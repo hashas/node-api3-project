9b10948e662bdb2b827bcbfb58ed4508bd946fe9
hh <hh@hhmbp15.local>
2020-04-27 00:57:21 +0100

    updated index.js and package.json to implement dotenv

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 // code away!
 const server = require('./server.js');
 
+// this will read all the environment variables
+// in '.env'
+// require("dotenv").config()
+
+// the above method could be problematic because
+// the .env has been added to gitignore and so will
+// not be pushed to github, therefore alternative is 
+// ammend package.json:
+// "scripts": { "watch": "nodemon -r dotenv/config index.js"}
+
 // import router from './users/userRouter.js'
 const userRouter = require('./users/userRouter.js')
 
-const port = 3000
+const port = process.env.PORT || 3000
 
 server.listen(port, () => {
     console.log(`Server is running on port ${port}`)
